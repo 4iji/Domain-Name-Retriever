7730c5c3481a98f200e4b533f823854aa333e6ba
vhir19 <vhirani19@gmail.com>
2016-07-01 20:01:23 +0530

    Working Code - Part 1

--- crawlers/main.py
+++ crawlers/main.py
@@ -3,6 +3,10 @@ import urllib2
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from difflib import SequenceMatcher
+from collections import defaultdict
+import neural_network as nn
+import numpy as np
+import operator
 
 import json
 with open('../config.json') as config_file:    
@@ -11,8 +15,26 @@ with open('../config.json') as config_file:
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 # Construct Feature Vector
-def constructFeatureMatrix(companyName,sifteryResults,googleResults):
-	return 1
+def constructInputMatrix(sifteryResults,googleResults,linkedinResults):
+	output = defaultdict(list)
+	for result in sifteryResults:
+		output[result].append(sifteryResults[result])
+	for result in googleResults:
+		if(not result in output):
+			output[result].append(0)
+		output[result].append(googleResults[result])
+	for result in linkedinResults:
+		if(not result in output):
+			output[result].append(0)
+		if(len(output[result])==1):
+			output[result].append(0)
+		output[result].append(linkedinResults[result])
+	for result in output:
+		if(len(output[result])==1):
+			output[result].append(0)
+		if(len(output[result])==2):
+			output[result].append(0)
+	return output
 # Siftery Crawler
 def crawlSiftery(browser,queryParam):
 	url = "https://siftery.com/search?q="+queryParam
@@ -21,18 +43,19 @@ def crawlSiftery(browser,queryParam):
 	companies = soup.find_all("div",{"class":"list-group-item__info"},limit=config['limit']['siftery'])
 	output = {}
 	for company in companies:
-		title = company.find('h5',{'class':'list-group-item__title'}).text
-		link = company.find('div',{'class':'list-group-item__text'}).text
-		link = link.split("//")[-1].split("/")[0]
-		output[link]=-1
-		output[link]=max(output[link],similar(title,queryParam))
+		try:
+			title = company.find('h5',{'class':'list-group-item__title'}).text
+			link = company.find('span',{'class':'primary-text ng-binding'}).text #skips product pages, only consider company pages
+			link = link.split("//")[-1].split("/")[0]
+			output[link]=-1
+			output[link]=max(output[link],round(similar(title,queryParam),2))
+		except AttributeError:
+			continue
 	return output
-
-
-		
 #Google Crawler
 def crawlGoogle(browser,queryParam):
-	url = "https://www.google.co.in/search?q="+queryParam+"&tbs=li:1" #The end part is to enable verbatim search
+	#Ignoring facebook,linkedin and twitter results
+	url = "https://www.google.co.in/search?q="+queryParam+"+-facebook+-twitter+-linkedin+-wikipedia&tbs=li:1" #The end part is to enable verbatim search
 	browser.get(url);
 	soup = BeautifulSoup(browser.page_source,"html.parser")
 	companies = soup.find_all('div',{'class':'g'})
@@ -42,7 +65,7 @@ def crawlGoogle(browser,queryParam):
 		link =  link.split("//")[-1].split("/")[0]
 		title = company.find('h3',{'class':'r'}).find('a').text
 		output[link]=-1
-		output[link]=max(output[link],similar(title,queryParam))
+		output[link]=max(output[link],round(similar(title,queryParam),2))
 	return output
 #LinkedIn Crawler
 def crawlLinkedin(browser,queryParam):
@@ -62,7 +85,7 @@ def crawlLinkedin(browser,queryParam):
 				link = newSoup.find("li",{"class":"website"}).find("a").text
 				link =  link.split("//")[-1].split("/")[0]
 				output[link]=-1
-				output[link]=max(output[link],similar(title,queryParam))
+				output[link]=max(output[link],round(similar(title,queryParam),2))
 			except AttributeError:
 				continue #skip to the next loop.
 	return output
@@ -74,4 +97,19 @@ companyName = raw_input("Please enter the name of the Company: ")
 sifteryVector = crawlSiftery(browser,companyName)
 googleVector = crawlGoogle(browser,companyName)
 linkedInVector = crawlLinkedin(browser,companyName)
+candidates = constructInputMatrix(sifteryVector,googleVector,linkedInVector)
+#Intialise a single neuron neural network.
+neural_network = nn.NeuralNetwork()
+training_set_inputs = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 1], [0, 0, 0]])
+training_set_outputs = np.array([[0, 1, 1, 0]]).T
+
+# Train the neural network using a training set.
+# Do it 10,000 times and make small adjustments each time.
+neural_network.train(training_set_inputs, training_set_outputs, 10000)
+# Test the neural network with a new situation.
+output = {}
+for result in candidates:
+	output[result] = neural_network.think(np.array(candidates[result]))
+sorted_output = sorted(output.items(), key=operator.itemgetter(1))
+print sorted_output
 browser.quit()
---
+++ crawlers/neural_network.py
@@ -0,0 +1,28 @@
+from numpy import exp, array, random, dot
+
+
+class NeuralNetwork():
+    def __init__(self):
+       
+        random.seed(1)
+        self.synaptic_weights = 2 * random.random((3, 1)) - 1
+
+   
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
+
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for iteration in xrange(number_of_training_iterations):
+            output = self.think(training_set_inputs)
+            error = training_set_outputs - output
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+            self.synaptic_weights += adjustment
+
+    def think(self, inputs):
+        # Pass inputs through the neural network (our single neuron).
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+
+   
