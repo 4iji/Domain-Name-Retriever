40cec5cabdfba8a9817f8af340bb0a81a7373dcb
vhir19 <vhirani19@gmail.com>
2016-07-01 05:30:22 +0530

    Removed obsolete crawlers and constructed feature vector for each crawler

--- crawlers/main.py
+++ crawlers/main.py
@@ -2,63 +2,76 @@
 import urllib2
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import json
+from difflib import SequenceMatcher
 
+import json
 with open('../config.json') as config_file:    
     config = json.load(config_file)
-# LevenShtein Distance 
-def levenshteinDistance(s1, s2):
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
-
-    distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
-    return distances[-1]
+# Similarity Score
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+# Construct Feature Vector
+def constructFeatureMatrix(companyName,sifteryResults,googleResults):
+	return 1
 # Siftery Crawler
 def crawlSiftery(browser,queryParam):
 	url = "https://siftery.com/search?q="+queryParam
 	browser.get(url);
 	soup = BeautifulSoup(browser.page_source,"html.parser")
 	companies = soup.find_all("div",{"class":"list-group-item__info"},limit=config['limit']['siftery'])
-	print companies
-	output = []
+	output = {}
 	for company in companies:
-		link = company.find('h5',{'class':'list-group-item__title'}).text
-		title = company.find('div',{'class':'list-group-item__text'}).text
-		pair = (title,link)
-		output.append(pair)
+		title = company.find('h5',{'class':'list-group-item__title'}).text
+		link = company.find('div',{'class':'list-group-item__text'}).text
+		link = link.split("//")[-1].split("/")[0]
+		output[link]=-1
+		output[link]=max(output[link],similar(title,queryParam))
 	return output
 
 
 		
 #Google Crawler
 def crawlGoogle(browser,queryParam):
-	url = "https://www.google.co.in/search?q="+response+"&tbs=li:1" #The end part is to enable verbatim search
+	url = "https://www.google.co.in/search?q="+queryParam+"&tbs=li:1" #The end part is to enable verbatim search
 	browser.get(url);
 	soup = BeautifulSoup(browser.page_source,"html.parser")
 	companies = soup.find_all('div',{'class':'g'})
-	output = []
+	output = {}
 	for company in companies:
 		link = company.find('cite').text
-		link =  link.split("/")[0]
+		link =  link.split("//")[-1].split("/")[0]
 		title = company.find('h3',{'class':'r'}).find('a').text
-		pair = (title,link)
-		output.append(pair)
+		output[link]=-1
+		output[link]=max(output[link],similar(title,queryParam))
 	return output
-
+#LinkedIn Crawler
+def crawlLinkedin(browser,queryParam):
+	url = "https://www.google.co.in/search?q="+queryParam+"+:www.linkedin.com/company"
+	browser.get(url);
+	soup = BeautifulSoup(browser.page_source,"html.parser")
+	companies = soup.find_all('div',{'class':'g'},limit=config['limit']['linkedin'])
+	output ={}
+	for company in companies:
+		linkedInURL = company.find('cite').text
+		linkedInURL_copy = linkedInURL
+		if linkedInURL_copy.split("//")[1].split("/")[0]=="www.linkedin.com" and linkedInURL_copy.split("//")[1].split("/")[1]=="company" :
+			browser.get(str(linkedInURL))
+			newSoup = BeautifulSoup(browser.page_source,"html.parser")
+			try:
+				title = newSoup.find("h1",{"class":"name"}).find('span').text
+				link = newSoup.find("li",{"class":"website"}).find("a").text
+				link =  link.split("//")[-1].split("/")[0]
+				output[link]=-1
+				output[link]=max(output[link],similar(title,queryParam))
+			except AttributeError:
+				continue #skip to the next loop.
+	return output
+			
 #Main code goes here
 PHANTOMJS_PATH = config['phantomJSPath']
 browser = webdriver.PhantomJS(PHANTOMJS_PATH)
-response = raw_input("Please enter the name of the Company: ")
-print crawlSiftery(browser,response)
-print crawlGoogle(browser,response)
-
-
+companyName = raw_input("Please enter the name of the Company: ")
+sifteryVector = crawlSiftery(browser,companyName)
+googleVector = crawlGoogle(browser,companyName)
+linkedInVector = crawlLinkedin(browser,companyName)
 browser.quit()
